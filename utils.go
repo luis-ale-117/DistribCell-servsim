0acd94842027b14ca454dd4cc4924a7c62b84f05
Luis Ale Mendoza <lalexmf117@gmail.com>
2023-10-29 18:36:27 -0600

    Add transaction when deleting queue item

--- utils.go
+++ utils.go
@@ -25,60 +25,68 @@ func gridToBytes(grid *cella.Grid) []byte {
 func processAutomaton(db *sql.DB, automaton *cella.Cella2d, simulation Simulaciones, lastGen Generaciones, proceso Cola) {
 	log.Printf("Total: %d Done: %d for %s", proceso.num_generaciones, lastGen.iteracion, simulation.nombre)
 	log.Printf("Updating job last update for %s cola %d simid %d", simulation.nombre, proceso.id, simulation.id)
-	query := `UPDATE cola SET ultima_actualizacion = ? WHERE id = ?;`
-	stmt, _ := db.Prepare(query)
-
-	_, err := stmt.Exec(time.Now().Unix(), proceso.id)
+	processError := false
+	query := "UPDATE cola SET ultima_actualizacion = ? WHERE id = ?"
+	_, err := db.Exec(query, time.Now().Unix(), proceso.id)
 	if err != nil {
 		log.Fatalf("Error executing query: %s", err)
 	}
-	stmt.Close()
+
 	for i := lastGen.iteracion; i < proceso.num_generaciones; i++ {
 		log.Printf("Processing generation %d of %d for %s", i, proceso.num_generaciones, simulation.nombre)
 		automaton.SetAuxBordersAsToroidal()
 		if automaton.NextGeneration() != nil {
-			log.Fatalf("Error processing generation %d for job %s", i, simulation.nombre)
+			log.Printf("Error processing generation %d for job %s", i, simulation.nombre)
+			processError = true
+			break
 		}
 
 		log.Printf("Saving generation %d of %d to database for job %s", i+1, proceso.num_generaciones, simulation.nombre)
 		content := automaton.GetNextGrid()
 		query := "INSERT INTO generaciones (simulacion_id, iteracion, contenido) VALUES (?, ?, ?)"
-		stmt, _ := db.Prepare(query)
-		_, err = stmt.Exec(simulation.id, i+1, gridToBytes(content))
+		_, err = db.Exec(query, i+1, gridToBytes(content))
 		if err != nil {
 			log.Fatalf("Error executing query: %s", err)
 		}
-		stmt.Close()
 
 		log.Printf("Updating job last update for %s cola %d simid %d", simulation.nombre, proceso.id, simulation.id)
 		query = "UPDATE cola SET ultima_actualizacion = ? WHERE id = ?"
-		stmt, _ = db.Prepare(query)
-		_, err = stmt.Exec(time.Now().Unix(), proceso.id)
+		_, err = db.Exec(query, time.Now().Unix(), proceso.id)
 		if err != nil {
 			log.Fatalf("Error executing query: %s", err)
 		}
-		stmt.Close()
 		automaton.InitGrid, automaton.NextGrid = automaton.NextGrid, automaton.InitGrid
 	}
+
+	// Start transaction
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatalf("Error executing query: %s", err)
+	}
+	defer tx.Rollback()
+
 	// Delete job from queue
 	log.Printf("Deleting process from queue for %s", simulation.nombre)
 	query = "DELETE FROM cola WHERE id = ?"
-	stmt, _ = db.Prepare(query)
-	_, err = stmt.Exec(proceso.id)
+	_, err = tx.Exec(query, proceso.id)
 	if err != nil {
 		log.Fatalf("Error executing query: %s", err)
 	}
-	stmt.Close()
 
 	// Update tipo from PROCESAMIENTO to PROCESADO in simulation
-	log.Printf("Updating simulation tipo to PROCESSED for %s %d", simulation.nombre, simulation.id)
-	query = "UPDATE simulaciones SET tipo = 'PROCESADO' WHERE id = ?"
-	stmt, _ = db.Prepare(query)
-	_, err = stmt.Exec(simulation.id)
+	tipo := "PROCESADO"
+	if processError {
+		tipo = "ERROR"
+	}
+	log.Printf("Updating simulation tipo to %s for %s %d", tipo, simulation.nombre, simulation.id)
+	query = "UPDATE simulaciones SET tipo = '" + tipo + "' WHERE id = ?"
+	_, err = tx.Exec(query, simulation.id)
 	if err != nil {
 		log.Fatalf("Error executing query: %s", err)
 	}
-	stmt.Close()
 
 	log.Println("Done")
+	if err = tx.Commit(); err != nil {
+		log.Fatalf("Error executing query: %s", err)
+	}
 }
